8f04125c6a73561001baa56c1de042a85f6f7f28
Peter Eichman <peichman@umd.edu>
2023-09-06 17:27:34 -0400

    LIBFCREPO-1061. Auto convert FCREPO_* environment variables to fcrepo.* system properties.
    - allows for easier configuration by just setting environment variables
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1061

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,22 @@
+# Convert any environment variables that start with FCREPO_ into
+# property declarations. Replaces "_" with ".", and transforms the
+# key into all lowercase.
+#
+# Examples::
+#
+#   # input environment
+#   FCREPO_HOME=/var/umd-fcrepo-webapp
+#   FCREPO_LOG_AUTH=DEBUG
+#
+#   # output line
+#    -Dfcrepo.home=/var/umd-fcrepo-webapp -Dfcrepo.log.auth=DEBUG
+#
+function get_env_opts {
+    env \
+    | grep -e '^FCREPO_' \
+    | while IFS='=' read -r key value; do printf " -D$(tr 'A-Z' 'a-z' <<<"$key" | tr '_' '.')=$value"; done
+}
+
 export CATALINA_OPTS="-XX:+UseConcMarkSweepGC \
   -XX:+CMSClassUnloadingEnabled \
   -XX:ConcGCThreads=5 \
@@ -10,4 +29,5 @@ export CATALINA_OPTS="-XX:+UseConcMarkSweepGC \
   -Dfcrepo.home=/var/umd-fcrepo-webapp \
   -Dfcrepo.activemq.directory=/var/activemq \
   -Dfcrepo.context.path=${CONTEXT_PATH} \
-  -Dconnection.timeout=${CONNECTION_TIMEOUT}"
+  -Dconnection.timeout=${CONNECTION_TIMEOUT} \
+  $(get_env_opts)"
